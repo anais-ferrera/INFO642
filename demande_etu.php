37d74108ed835b0bf8c3d47e9f898844cd10d0f5
anais-ferrera <56952574+anais-ferrera@users.noreply.github.com>
2020-04-29 12:07:00 +0200

    Add files via upload

--- demande_etu.php
+++ demande_etu.php
@@ -1,42 +1,51 @@
 <?php
+include ("connexion_bdd.php");
+
+if (issett($_POST['des_demande']))
+{
+	$sql="INSERT INTO Demande(description) VALUES '".$_POST['des_demande']."'";
+	$result = mysql_query($sql) or die("Requête invalide: ". mysql_error()."\n".$sql);
+}
 echo "
 <html>
 
-//On cree un formulaire où on passe les donnees par la methode post
+<!--On cree un formulaire où on passe les donnees par la methode post -->
 <form method='post' action ='demande_2_etu.php'>
 
+<!--Passage cache de l'id_demande -->
+<input type='hidden' name='id' value='$id_demande'>
 
-//Zone de saisie du code APP 
+<!--Zone de saisie du code APP -->
 <p><label>Code APP</label> : <input type='text' name='codeAPP'/></p>
 
-//Zone de saisie du fournisseur
+<!--Zone de saisie du fournisseur -->
 <p><label>Fournisseur</label> : <input type='text' name='fournisseur'/></p>
 
-//Zone de saisie de la quantite utile
-<p><label>Quantité utile</label> : <input type='number' name='quantiteUtil'/></p>
+<!--Zone de saisie de la quantite utile -->
+<p><label>Quantite utile</label> : <input type='number' name='quantiteUtil'/></p>
 
-//Zone de saisie de la quantite commande
+<!--Zone de saisie de la quantite commande -->
 <p><label>Quantite commande</label> : <input type='number' name='quantiteCommande'/></p>
 
-//Zone de saisie du nom
+<!--Zone de saisie du nom -->
 <p><label>Nom</label> : <input type='text' name='nom'/></p>
 
-//Zone de saisie du lien
+<!--Zone de saisie du lien -->
 <p><label>Lien</label> : <input type='text' name='lien'/></p>
 
-//Zone de saisie de la description
+<!--Zone de saisie de la description -->
 <p><label>Description</label> : <input type='textarea' name='description'/></p>
 
-//Zone de saisie du prix unitaire TTC
+<!--Zone de saisie du prix unitaire TTC -->
 <p><label>Prix unitaire TTC</label> : <input type='number' name='puTTC'/></p>
 
-//Zone de saisie du montant TTC
+<!--Zone de saisie du montant TTC -->
 <p><label>Montant TTC</label> : <input type='number' name='montantTTC'/></p>
 
-//Zone d'ajout du document
-<p><label>Document</label> : <input type='number' name='quantiteCommande'/></p>
+<!--Zone d'ajout du document -->
+<p><label>Document</label> : <input type='number' name='document'/></p>
 
-//Bouton pour envoyer les donnees
+<!--Bouton pour envoyer les donnees-->
 <input type='submit' value='Envoyer' />
 </form>
 
