cce4beea90848e1c4e595d27ed7bad38537ae1e4
saphya-el-ouahabi <56952563+saphya-el-ouahabi@users.noreply.github.com>
2020-04-29 12:03:32 +0200

    Add files via upload

--- accueil.php
+++ accueil.php
@@ -1,3 +1,15 @@
+<!DOCTYPE HTML>
+
+<html>
+  <head>
+    <title></title>
+    <meta content="info">
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="accueil.css" />
+  </head>
+  
+
+
 <?php
 
   echo "<h2>Bienvenue sur notre site de gestion des commandes pour projets pédagogiques</h2>\n";
@@ -17,8 +29,50 @@ Votre mot de passe : <input type='password' name='pwd'><br />
 </form>
 
 </body>
-</html>\n";
+
+</html>";
   
 
 
 ?>
+
+
+
+<marquee behavior="scroll" align="center" direction="right" scrollamount="8" height="300" 
+			scrolldelay="1" onmouseover='this.stop()' onmouseout='this.start()'>
+			
+		<img src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.polytech.univ-smb.fr%2F&psig=AOvVaw3LNKuPkECkzu4NFZNpbdWe&ust=1588240242890000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCLDEro-ujekCFQAAAAAdAAAAABAD" >
+			
+	    <img src="https://www.univ-smb.fr/wp-content/uploads/2015/04/polytech.jpg">
+			
+		<img  src="https://www.polytech-reseau.org/fileadmin/GridElements/Entetes/polytech_annecy_chambery.jpg" >
+		
+		<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcS213XQ9W7QSgi7UVKLyus-DqRjqsbsI-mnrqdnmQXDK1ta-HW6&usqp=CAU" >
+
+		<img src="https://www.univ-smb.fr/wp-content/uploads/2017/09/
+		philippe_bolon_nouveau_directeur_polytech_yannick_perrin_.jpg3">
+		
+</marquee>
+
+
+
+<section id="contact">
+                           <h2>Contact</h2>
+                           <address>
+                           <ul>
+                           <li>06 66 66 66 66</li>
+                           <li><a href="mailto:lesmagiciens@gmail.com">lesmagiciens@gmail.com</a></li>
+                           </ul>
+                           </address>
+                </section>
+
+
+    <div id="pied">
+      <span>Polytech Annecy-Chambéry - Module Info642- Base de données et Technologies web</span>
+    </div>
+ 
+
+</html>
+
+
+
