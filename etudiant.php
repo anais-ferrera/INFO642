c881bf60a88d40576189291abf858d963e3fc945
anais-ferrera <56952574+anais-ferrera@users.noreply.github.com>
2020-05-04 15:32:16 +0200

    Add files via upload

--- etudiant.php
+++ etudiant.php
@@ -12,6 +12,8 @@ echo"
 
 <h1>Faire une demande </h1>
 <form action ='demande_etu.php' method='post'>
+<!--Zone de desscription de la demande-->
+<p><label>Description de la demande</label> : <input type='textarea' name='des_demande' /></p>
 <input type='submit' value='Faire une demande'>
 </form>
 
