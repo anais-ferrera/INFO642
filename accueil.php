8948814a43ef3665c08dc2078f49b88a479b8357
saphya-el-ouahabi <56952563+saphya-el-ouahabi@users.noreply.github.com>
2020-05-04 16:06:51 +0200

    Add files via upload

--- accueil.php
+++ accueil.php
@@ -2,11 +2,12 @@
 
 <html>
   <head>
-    <title></title>
-    <meta content="info">
     <meta charset="UTF-8">
-    <link rel="stylesheet" href="accueil.css" />
-  </head>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"> </script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"> </script>
+</head>
   
 
 
@@ -33,26 +34,7 @@ Votre mot de passe : <input type='password' name='pwd'><br />
 </html>";
   
 
-
 ?>
-
-
-
-<marquee behavior="scroll" align="center" direction="right" scrollamount="8" height="300" 
-			scrolldelay="1" onmouseover='this.stop()' onmouseout='this.start()'>
-			
-		<img src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.polytech.univ-smb.fr%2F&psig=AOvVaw3LNKuPkECkzu4NFZNpbdWe&ust=1588240242890000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCLDEro-ujekCFQAAAAAdAAAAABAD" >
-			
-	    <img src="https://www.univ-smb.fr/wp-content/uploads/2015/04/polytech.jpg">
-			
-		<img  src="https://www.polytech-reseau.org/fileadmin/GridElements/Entetes/polytech_annecy_chambery.jpg" >
-		
-		<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcS213XQ9W7QSgi7UVKLyus-DqRjqsbsI-mnrqdnmQXDK1ta-HW6&usqp=CAU" >
-
-		<img src="https://www.univ-smb.fr/wp-content/uploads/2017/09/
-		philippe_bolon_nouveau_directeur_polytech_yannick_perrin_.jpg3">
-		
-</marquee>
 
 
 
@@ -67,12 +49,83 @@ Votre mot de passe : <input type='password' name='pwd'><br />
                 </section>
 
 
+  
+
+<body>
+
+<div class="container">
+  <div id="myCarousel" class="carousel slide" data-ride="carousel">
+    <!-- Indicators -->
+    <ol class="carousel-indicators">
+      <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+      <li data-target="#myCarousel" data-slide-to="1"></li>
+      <li data-target="#myCarousel" data-slide-to="2"></li>
+      <li data-target="#myCarousel" data-slide-to="3"></li>
+    </ol>
+
+    <!-- Wrapper for slides -->
+    <div class="carousel-inner">
+
+      <div class="item active">
+        <img src="photo_lac.jpg" alt="lac" style="width:100%;">
+        <div class="carousel-caption">
+          <h3>Le lac d'Annecy</h3>
+        </div>
+      </div>
+
+      <div class="item">
+        <img src="philippe_bolon.jpg" alt="philippe" style="width:100%;">
+        <div class="carousel-caption">
+          <h3>Le directeur de Polytech Annecy-Chambéry</h3>
+          <p>Philippe Bolon</p>
+        </div>
+      </div>
+    
+      <div class="item">
+        <img src="escalier_polytech.jpg" alt="escalier" style="width:100%;">
+        <div class="carousel-caption">
+          <h3>L'établissement Polytech Annecy-Chambéry</h3>
+          <p>Campus Annecy-le-Vieux</p>
+        </div>
+      </div>
+
+      <div class="item">
+        <img src="etudiants_ceremonie_rentree.jpg" alt="ceremonie" style="width:100%;">
+        <div class="carousel-caption">
+          <h3>La cérémonie de rentrée</h3>
+          <p>A Annecy-le-Vieux</p>
+        </div>
+      </div>      
+  
+    </div>
+
+    <!-- Left and right controls -->
+    <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+      <span class="glyphicon glyphicon-chevron-left"></span>
+      <span class="sr-only">Previous</span>
+    </a>
+    <a class="right carousel-control" href="#myCarousel" data-slide="next">
+      <span class="glyphicon glyphicon-chevron-right"></span>
+      <span class="sr-only">Next</span>
+    </a>
+  </div>
+</div>
+
+</body>
+
+
+
+
+
+
+
     <div id="pied">
       <span>Polytech Annecy-Chambéry - Module Info642- Base de données et Technologies web</span>
+      <img src="logo_polytech_annecy_chambery.jpg" alt="logo" style="width:10%;">
     </div>
- 
+
+
 
 </html>
-
 
 
