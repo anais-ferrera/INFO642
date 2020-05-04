599ffb18b0a355bc3986acfa5d49cc33934883dc
anais-ferrera <56952574+anais-ferrera@users.noreply.github.com>
2020-05-04 16:00:21 +0200

    Add files via upload

--- demande_2_etu.php
+++ demande_2_etu.php
@@ -4,10 +4,10 @@ include("connexion_bdd.php");
 
 //On insere une nouvelle demande dans la base
 //On verifie que tous les champs ont ete renseignes 
-if(isset($_POST['codeAPP']) and isset($_POST['fournisseur'])and isset($_POST['quantiteUtil']) and isset($_POST['quantiteCommande']) and isset($_POST['nom']) and isset($_POST['lien']) and isset($_POST['puTTC']) and isset($_POST['montantTTC'])){
+if(($_POST['codeAPP'])!="" and ($_POST['fournisseur'])!="" and ($_POST['quantiteUtil'])!="" and ($_POST['quantiteCommande'])!="" and ($_POST['nom'])!="" and ($_POST['lien'])!="" and ($_POST['description'])!="" and ($_POST['puTTC'])!="" and ($_POST['montantTTC'])!=""){
 	//$sql="INSERT INTO Demande(description) VALUES ('".$_POST['des_demande']."')";
 	//On insere dans la table capteur le nouveau capteur avec les valeurs renseignees par l'utilisateur
-	$sql="INSERT INTO Produit (CodeAPP,fournisseur,quantiteUtil,quantiteCommande,nom,lien,puTTC,montantTTC,id_demande) VALUES ('".$_POST['codeAPP']."','".$_POST['fournisseur']."','".$_POST['quantiteUtil']."','".$_POST['quantiteCommande']."','".$_POST['nom']."','".$_POST['lien']."','".$_POST['puTTC']."','".$_POST['montantTTC']."','".$_POST['id']."')";
+	$sql="INSERT INTO Produit (CodeAPP,fournisseur,quantiteUtil,quantiteCommande,nom,lien,description,puTTC,montantTTC,id_demande) VALUES ('".$_POST['codeAPP']."','".$_POST['fournisseur']."','".$_POST['quantiteUtil']."','".$_POST['quantiteCommande']."','".$_POST['nom']."','".$_POST['lien']."','".$_POST['description']."','".$_POST['puTTC']."','".$_POST['montantTTC']."','".$_POST['id']."')";
 	$result = mysql_query($sql) or die("Requête invalide: ". mysql_error()."\n".$sql);
 	echo'La demande a bien ete envoyee.';
 
