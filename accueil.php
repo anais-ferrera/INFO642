f71d155bc2a8959fb8165a48901454e8f83534c2
saphya-el-ouahabi <56952563+saphya-el-ouahabi@users.noreply.github.com>
2020-05-31 18:31:59 +0200

    Add files via upload

--- accueil.php
+++ accueil.php
@@ -2,37 +2,84 @@
 
 <html>
   <head>
+    <title> Accueil </title>
+    <meta content="info">
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"> </script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"> </script>
-</head>
-  
+    <link rel="stylesheet" href="Style.css" />
+  </head>
+
+ 
+ <body>
+
+    <div id="banniere1">
+      <p>Gestion des commandes</p>
+    </div>
+
+    <div id="menu">
+    </div>
+
+    <div id="banniere2">  
+    </div> 
 
 
+    <div id="contenu">
+
+    <h1>Formulaire d'identification</h1>
 <?php
-
-  echo "<h2>Bienvenue sur notre site de gestion des commandes pour projets pédagogiques</h2>\n";
   
-  //Formulaire de connexion au site
-  echo "<html>
-<head>
-<title>Formulaire d'identification</title>
-</head>
-<body>
-<h1>Connexion au site</h1>
-<form action='login.php' method='post'>
-Votre login : <input type='text' name='login'><br />
-Votre mot de passe : <input type='password' name='pwd'><br />
-<input type='submit' value='Connexion'>
-</form>
-</body>
-</html>";
-  
+      //Formulaire de connexion au site
+      echo "<html>
 
+    <form action='login.php' method='post'>
+
+    Votre login : <input type='text' name='login'><br />
+    Votre mot de passe : <input type='password' name='pwd'><br />
+
+    <input type='submit' value='Connexion'>
+    </form>
+
+    </html>" 
 ?>
 
+</div>
+ 
+<div id="slider">
+  <input type="radio" id="show_slide1" name="slider_commands">
+  <input type="radio" id="show_slide2" name="slider_commands">
+  <input type="radio" id="show_slide3" name="slider_commands">
+
+
+  <input type="radio" id="play_slider" name="slider_commands">
+
+  <div id="slides">
+
+    <figure id="slide1">
+      <img src="./images/lac_annecy.jpg" alt="lac">
+      <figcaption>Lac d'Annecy</figcaption>
+    </figure>
+
+    <figure id="slide2">
+      <img src="./images/etudiants_ceremonie_rentree.jpg" alt="ceremonie">
+      <figcaption>Cérémonie de rentrée à Annecy-le-Vieux</figcaption>
+    </figure>
+
+    <figure id="slide3">
+      <img src="./images/geo.jpg" alt="transition">
+    </figure>
+
+
+
+  </div>
+  <nav>
+    <ul class="dots_commands">
+      <li><label for="show_slide1">Slide 1</label></li>
+      <li><label for="show_slide2">Slide 2</label></li>
+      <li><label for="show_slide3">Slide 3</label></li>
+    </ul>
+    <label for="play_slider" aria-label="Play" id="play" title="Play">Play</label>
+  </nav>
+</div>
+
 
 
 <section id="contact">
@@ -40,128 +87,22 @@ Votre mot de passe : <input type='password' name='pwd'><br />
                            <address>
                            <ul>
                            <li>06 66 66 66 66</li>
-                           <li><a href="mailto:lesmagiciens@gmail.com">lesmagiciens@gmail.com</a></li>
+                           <li><a href="mailto:lesmagiciennesIDU@gmail.com">lesmagiciennesIDU@gmail.com</a></li>
                            </ul>
                            </address>
-                </section>
+</section>
 
 
-  
 
-<body>
 
-<div class="container">
-  <div id="myCarousel" class="carousel slide" data-ride="carousel">
-    <!-- Indicators -->
-    <ol class="carousel-indicators">
-      <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
-      <li data-target="#myCarousel" data-slide-to="1"></li>
-      <li data-target="#myCarousel" data-slide-to="2"></li>
-      <li data-target="#myCarousel" data-slide-to="3"></li>
-    </ol>
+<div id="footer">
+      <p>Polytech Annecy-Chambéry - Module INFO642 - Base de données et Technologies web </p>
 
-    <!-- Wrapper for slides -->
-    <div class="carousel-inner">
-
-      <div class="item active">
-        <img src="photo_lac.jpg" alt="lac" style="width:100%;">
-        <div class="carousel-caption">
-          <h3>Le lac d'Annecy</h3>
-        </div>
-      </div>
-
-      <div class="item">
-        <img src="philippe_bolon.jpg" alt="philippe" style="width:100%;">
-        <div class="carousel-caption">
-          <h3>Le directeur de Polytech Annecy-Chambéry</h3>
-          <p>Philippe Bolon</p>
-        </div>
-      </div>
-    
-      <div class="item">
-        <img src="escalier_polytech.jpg" alt="escalier" style="width:100%;">
-        <div class="carousel-caption">
-          <h3>L'établissement Polytech Annecy-Chambéry</h3>
-          <p>Campus Annecy-le-Vieux</p>
-        </div>
-      </div>
-
-      <div class="item">
-        <img src="etudiants_ceremonie_rentree.jpg" alt="ceremonie" style="width:100%;">
-        <div class="carousel-caption">
-          <h3>La cérémonie de rentrée</h3>
-          <p>A Annecy-le-Vieux</p>
-        </div>
-      </div>      
-  
-    </div>
-
-    <!-- Left and right controls -->
-    <a class="left carousel-control" href="#myCarousel" data-slide="prev">
-      <span class="glyphicon glyphicon-chevron-left"></span>
-      <span class="sr-only">Previous</span>
-    </a>
-    <a class="right carousel-control" href="#myCarousel" data-slide="next">
-      <span class="glyphicon glyphicon-chevron-right"></span>
-      <span class="sr-only">Next</span>
-    </a>
-  </div>
 </div>
+
 
 </body>
 
-
-
-
-
-
-
-    <div id="pied">
-      <span>Polytech Annecy-Chambéry - Module Info642- Base de données et Technologies web</span>
-      <img src="logo_polytech_annecy_chambery.jpg" alt="logo" style="width:10%;">
-    </div>
-
-
-
 </html>
-
-
-
-<marquee behavior="scroll" align="center" direction="right" scrollamount="8" height="300" 
-			scrolldelay="1" onmouseover='this.stop()' onmouseout='this.start()'>
-			
-		<img src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.polytech.univ-smb.fr%2F&psig=AOvVaw3LNKuPkECkzu4NFZNpbdWe&ust=1588240242890000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCLDEro-ujekCFQAAAAAdAAAAABAD" >
-			
-	    <img src="https://www.univ-smb.fr/wp-content/uploads/2015/04/polytech.jpg">
-			
-		<img  src="https://www.polytech-reseau.org/fileadmin/GridElements/Entetes/polytech_annecy_chambery.jpg" >
-		
-		<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcS213XQ9W7QSgi7UVKLyus-DqRjqsbsI-mnrqdnmQXDK1ta-HW6&usqp=CAU" >
-
-		<img src="https://www.univ-smb.fr/wp-content/uploads/2017/09/
-		philippe_bolon_nouveau_directeur_polytech_yannick_perrin_.jpg3">
-		
-</marquee>
-
-
-
-<section id="contact">
-                           <h2>Contact</h2>
-                           <address>
-                           <ul>
-                           <li>06 66 66 66 66</li>
-                           <li><a href="mailto:lesmagiciens@gmail.com">lesmagiciens@gmail.com</a></li>
-                           </ul>
-                           </address>
-                </section>
-
-
-    <div id="pied">
-      <span>Polytech Annecy-Chambéry - Module Info642- Base de données et Technologies web</span>
-    </div>
- 
-
-</html>
-
 
 
