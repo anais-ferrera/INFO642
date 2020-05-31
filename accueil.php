2c3dd42a23ba6187c172bd7c9fc4e088d9fc2a8c
saphya-el-ouahabi <56952563+saphya-el-ouahabi@users.noreply.github.com>
2020-05-31 22:35:50 +0200

    Add files via upload

--- accueil.php
+++ accueil.php
@@ -3,15 +3,18 @@
 <html>
   <head>
     <title> Accueil </title>
-    <meta content="info">
     <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"> </script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"> </script>
     <link rel="stylesheet" href="Style.css" />
-  </head>
+</head>
+  
+<body>
 
- 
- <body>
 
-    <div id="banniere1">
+   <div id="banniere1">
       <p>Gestion des commandes</p>
     </div>
 
@@ -25,68 +28,80 @@
     <div id="formulaire">
 
     <h1>Formulaire d'identification</h1>
-<?php
-  
-      //Formulaire de connexion au site
-      echo "<html>
 
-    <form action='login.php' method='post'>
-
-    Votre login : <input type='text' name='login'><br />
-    Votre mot de passe : <input type='password' name='pwd'><br />
-
-    <input type='submit' value='Connexion'>
-    </form>
-
-    </html>" 
-?>
-
-</div>
-
-    <div id="banniere2">  
-    </div> 
- 
-<div id="slider">
-  <input type="radio" id="show_slide1" name="slider_commands">
-  <input type="radio" id="show_slide2" name="slider_commands">
-  <input type="radio" id="show_slide3" name="slider_commands">
-
-
-  <input type="radio" id="play_slider" name="slider_commands">
-
-  <div id="slides">
-
-    <figure id="slide1">
-      <img src="./images/lac_annecy.jpg" alt="lac">
-      <figcaption>Lac d'Annecy</figcaption>
-    </figure>
-
-    <figure id="slide2">
-      <img src="./images/etudiants_ceremonie_rentree.jpg" alt="ceremonie">
-      <figcaption>Cérémonie de rentrée à Annecy-le-Vieux</figcaption>
-    </figure>
-
-    <figure id="slide3">
-      <img src="./images/transition.jpg" alt="transition">
-    </figure>
-
+<form action='login.php' method='post'>
+Votre login : <input type='text' name='login'><br />
+Votre mot de passe : <input type='password' name='pwd'><br />
+<input type='submit' value='Connexion'>
+</form>
 
 
   </div>
-  <nav>
-    <ul class="dots_commands">
-      <li><label for="show_slide1">Slide 1</label></li>
-      <li><label for="show_slide2">Slide 2</label></li>
-      <li><label for="show_slide3">Slide 3</label></li>
-    </ul>
-    <label for="play_slider" aria-label="Play" id="play" title="Play">Play</label>
-  </nav>
-</div>
 
-   
     <div id="banniere2">  
     </div> 
 
+
+<div class="container">
+  <div id="myCarousel" class="carousel slide" data-ride="carousel">
+    <!-- Indicators -->
+    <ol class="carousel-indicators">
+
+      <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+      <li data-target="#myCarousel" data-slide-to="1"></li>
+      <li data-target="#myCarousel" data-slide-to="2"></li>
+      <li data-target="#myCarousel" data-slide-to="3"></li>
+
+    </ol>
+
+    <!-- Wrapper for slides -->
+    <div class="carousel-inner">
+
+      <div class="item active">
+        <img src="./images/photo_lac_annecy2.jpg" alt="lac" style="width:100%;">
+        <div class="carousel-caption">
+          <h3>Le lac d'Annecy</h3>
+        </div>
+      </div>
+
+      <div class="item">
+        <img src="./images/photo_montagnes2.jpg" alt="montagnes" style="width:100%;">
+        <div class="carousel-caption">
+        </div>
+      </div>
+
+      <div class="item">
+        <img src="./images/photo_lac_annecy.jpg" alt="lac" style="width:100%;">
+        <div class="carousel-caption">
+          <h3>Le lac d'Annecy</h3>
+        </div>
+      </div>
+    
+      <div class="item">
+        <img src="./images/photo_montagnes2.jpg" alt="montagnes" style="width:100%;">
+        <div class="carousel-caption">
+        </div>
+      </div>
+
+  
+    </div>
+
+    <!-- Left and right controls -->
+    <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+      <span class="glyphicon glyphicon-chevron-left"></span>
+      <span class="sr-only">Previous</span>
+    </a>
+    <a class="right carousel-control" href="#myCarousel" data-slide="next">
+      <span class="glyphicon glyphicon-chevron-right"></span>
+      <span class="sr-only">Next</span>
+    </a>
+  </div>
+</div>
+
+
+
+    <div id="banniere2">  
+    </div> 
 
 <div id="contact">
                            <h2>Un problème de connexion ? </h2>
@@ -98,10 +113,10 @@
 </div>
 
 
-
-
 <div id="footer">
       <p>Polytech Annecy-Chambéry - Module INFO642 - Base de données et Technologies web </p>
+      <img src="./images/logo.jpg" />
+      </p>
 
 </div>
 
@@ -109,5 +124,6 @@
 </body>
 
 </html>
+
 
 
