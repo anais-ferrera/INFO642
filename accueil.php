1f85ba4614e1c956cdc8576412353f811fa5ae36
saphya-el-ouahabi <56952563+saphya-el-ouahabi@users.noreply.github.com>
2020-06-01 11:21:04 +0200

    Update accueil.php

--- accueil.php
+++ accueil.php
@@ -12,64 +12,45 @@
 </head>
   
 <body>
-
-
    <div id="banniere1">
       <p>Gestion des commandes</p>
     </div>
-
     <div id="menu">
     </div>
-
     <div id="banniere2">  
     </div> 
-
-
     <div id="formulaire">
-
     <h1>Formulaire d'identification</h1>
-
 <form action='login.php' method='post'>
 Votre login : <input type='text' name='login'><br />
 Votre mot de passe : <input type='password' name='pwd'><br />
 <input type='submit' value='Connexion'>
 </form>
-
-
   </div>
-
     <div id="banniere2">  
     </div> 
-
-
 <div class="container">
   <div id="myCarousel" class="carousel slide" data-ride="carousel">
     <!-- Indicators -->
     <ol class="carousel-indicators">
-
       <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
       <li data-target="#myCarousel" data-slide-to="1"></li>
       <li data-target="#myCarousel" data-slide-to="2"></li>
       <li data-target="#myCarousel" data-slide-to="3"></li>
-
     </ol>
-
     <!-- Wrapper for slides -->
     <div class="carousel-inner">
-
       <div class="item active">
         <img src="./images/photo_lac_annecy2.jpg" alt="lac" style="width:100%;">
         <div class="carousel-caption">
           <h3>Le lac d'Annecy</h3>
         </div>
       </div>
-
       <div class="item">
         <img src="./images/photo_montagnes2.jpg" alt="montagnes" style="width:100%;">
         <div class="carousel-caption">
         </div>
       </div>
-
       <div class="item">
         <img src="./images/photo_lac_annecy.jpg" alt="lac" style="width:100%;">
         <div class="carousel-caption">
@@ -82,10 +63,8 @@ Votre mot de passe : <input type='password' name='pwd'><br />
         <div class="carousel-caption">
         </div>
       </div>
-
   
     </div>
-
     <!-- Left and right controls -->
     <a class="left carousel-control" href="#myCarousel" data-slide="prev">
       <span class="glyphicon glyphicon-chevron-left"></span>
@@ -97,12 +76,8 @@ Votre mot de passe : <input type='password' name='pwd'><br />
     </a>
   </div>
 </div>
-
-
-
     <div id="banniere2">  
     </div> 
-
 <div id="contact">
                            <h2>Un problème de connexion ? </h2>
                            <b>Veuillez prendre contact avec les créatrices de ce site </b>
@@ -111,19 +86,13 @@ Votre mot de passe : <input type='password' name='pwd'><br />
                            <li><a href="mailto:lesmagiciennesIDU@gmail.com">lesmagiciennesIDU@gmail.com</a></li>
                            </ul>
 </div>
+    <div id="banniere2">  
+    </div> 
 
-
-<div id="footer">
+<div id="footer2">
       <p>Polytech Annecy-Chambéry - Module INFO642 - Base de données et Technologies web </p>
       <img src="./images/logo.jpg" />
       </p>
-
 </div>
-
-
 </body>
-
 </html>
-
-
-
