e646f826f81797fa6f562a68a0463aff6ef579df
anais-ferrera <56952574+anais-ferrera@users.noreply.github.com>
2020-06-01 10:14:16 +0200

    Add files via upload

--- modif_etu.php
+++ modif_etu.php
@@ -2,21 +2,21 @@
 session_start();
 include ("connexion_bdd.php");
 
-//On selectionne le colonne nom de la table capteur
-$sql ="SELECT id_demande, description from Demande WHERE etat='En cours'";
+$sql =  "SELECT Demande.id_demande as id_demande, Demande.description as description FROM Demande,passe,Etudiant where Demande.id_demande=passe.id_demande and Etudiant.id_etu=passe.id_etu and Etudiant.mail='".$_SESSION["login"]."' and Demande.etat !='enAttenteDuST' ";
+
 $result = mysql_query($sql) or die("Requête invalide: ". mysql_error()."\n".$sql);
 echo '<h2>Modification d\'une demande <br/></h2>';
 
 //On cree un formulaire avec la methode post
 echo '<form method="post" action="modif_2_etu.php">';
-//Liste deroulante avec les differents noms de capteurs presents dans la bdd
+//Liste deroulante avec les differentes demandes de l'etudiant presents dans la bdd
 echo '<label>Demandes faites</label> : <select name="demande">';
         
         while($row =mysql_fetch_assoc($result)) {
             echo '<option value='.$row["id_demande"].'>'.$row["description"].'</option>';
         }
         echo '</select>';
-//Bouton qui permet de selectionner un capteur
+//Bouton qui permet de selectionner une demande
 echo'<input type="submit" name="modifier" value="Modifier" />';
 echo'</form>';
 
