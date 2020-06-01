642d734eeb94f1b3549814263c0110c82206219e
anais-ferrera <56952574+anais-ferrera@users.noreply.github.com>
2020-06-01 11:24:40 +0200

    Add files via upload

--- page_0.inc.php
+++ page_0.inc.php
@@ -1,32 +1,86 @@
-<?php 
-  $login= $_SESSION["login"];
-  echo "<html>
-<head>
-<title>Formulaire d'identification</title>
-</head>
-<body>
-<h1>Voici les demandes passées :</h1>
-</body>
-</html>\n";
+<?php
+echo'
+<!DOCTYPE html>
+<html>
 
-    include("connexion_bdd.php");
-
-    $sql =  "SELECT Demande.description as d_description, Demande.etat as d_etat FROM Demande,passe,Etudiant where Demande.id_demande=passe.id_demande and Etudiant.id_etu=passe.id_etu and Etudiant.mail='".$login."' ";
+  <head>
+    <meta charset="utf-8" />
+    <link rel="stylesheet" type="text/css" href="Style.css" />
+      <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+      <title>Demande en cours</title>
+  </head>
 
 
-    $result = mysql_query($sql) or die("Requête invalide: ". mysql_error()."\n".$sql);
+  <body>
 
-    echo "<table border='1'>";
-         echo "<tr>";
-            echo "<td><b>Demandes</td></b>";
-			echo "<td><b>Etat</td></b>";
-        echo"</tr>";
-        while ($row = mysql_fetch_assoc($result)) { 
-           echo "<tr>";
+    <div id="banniere1ETU">
+      <p>Gestion des commandes</p>
+    </div>
+
+    <div id="menuETU">
+      <ul>
+        <li><a href="demande_en_cours.html">Demande en cours</a></li> <!-- sinon on voit plus "acceuil" ds le menu qd on est dans acceuil -->         
+        <li><a href="etu_creer_demande.html">Creer une demande</a></li>
+        <li><a href="etu_modif_demande.html">Modifier</a></li>
+        <li><a href="messagerie.html">Messagerie</a></li>
+        <li><a href="page_connexion.html">Deconnexion</a>
+
+        </li>
+        
+      </ul>
+    </div>
+
+    <div id="banniere2ETU"> 
+    </div>
+    
+
+    <div id="contenu">
+ 
+
+      <h1>Demande en cours </h1>
+
+      <br/> 
+
+      <p>Voici les demandes en cours de validation : </p>
+
+      <br/>';
+
+      
+
+      include("connexion_bdd.php");
+
+        $sql =  "SELECT Demande.description as d_description, Demande.etat as d_etat 
+            FROM Demande,passe,Etudiant 
+            where Demande.id_demande=passe.id_demande 
+            and 1=passe.id_etu";
+
+
+        $result = mysql_query($sql) or die("Requête invalide: ". mysql_error()."\n".$sql);
+
+        echo "<table border='1'>";
+          echo "<tr>";
+            echo "<td><b>demandes</td></b>";
+          echo"</tr>";
+          while ($row = mysql_fetch_assoc($result)) { 
+            echo "<tr>";
                echo "<td>".$row["d_description"]."</td>";
                echo "<td>".$row["d_etat"]."</td>";
             echo "</tr>";
         }
-    echo "</table>";
+      echo '</table>
+    
 
-?>
+
+    </div>
+    
+
+    <div id="footer">
+      <p>Polytech Annecy-Chambéry - Module INFO642 - Base de données et Technologies web </p>
+
+    </div>
+
+  </body>
+
+
+</html>';
+
